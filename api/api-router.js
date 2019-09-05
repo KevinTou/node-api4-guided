ee2d4b5c07c7770a729704f22f090ab1dcaeefb8
Kevin Tou <toukevin@gmail.com>
2019-09-05 09:44:17 -0700

    Logging NODE_ENV environment variable

--- api/api-router.js
+++ api/api-router.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 router.use(express.json());
 
+// GET /api
 router.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
 });
 
+// GET /api/shouts
 router.get('/shouts', (req, res, next) => {
   Shouts.find()
     .then(shouts => {
@@ -18,6 +20,7 @@ router.get('/shouts', (req, res, next) => {
     .catch(error => next(error));
 });
 
+// POST /api/shouts
 router.post('/shouts', (req, res, next) => {
   Shouts.add(req.body)
     .then(shout => {
